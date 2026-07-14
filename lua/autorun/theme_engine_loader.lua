c0598fb94081d124bd5ad431ae75d126453f9bc5
Remedypp <138019844+Remedypp@users.noreply.github.com>
2026-07-14 16:11:44 -0400

    Delete lua directory

--- lua/autorun/theme_engine_loader.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-    for _, fname in ipairs(file.Find("theme_engine/*.lua", "LUA") or {}) do
-        AddCSLuaFile("theme_engine/" .. fname)
-    end
-end
-
-if CLIENT then
-    include("theme_engine/theme_engine_spawnmenu.lua")
-end
